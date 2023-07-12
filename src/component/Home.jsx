a183940933adb4a8c0219b24ea535422cbd70fad
Chandrakant <chandrakantgawali92@gmail.com>
2023-07-12 13:18:29 +0530

    Update

---
+++ src/component/Home.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import { NavLink } from "react-router-dom";
+
+export class Home extends Component {
+    static propTypes = {}
+
+    render() {
+        return (
+            <>
+            <div></div>
+               
+               
+            </>
+
+
+        )
+    }
+}
+
+export default Home
