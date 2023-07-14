1fde159b67cbadb2ad900db12d5bb1b58168e99c
Chandrakant <chandrakantgawali92@gmail.com>
2023-07-14 16:34:38 +0530

    add slider page

--- src/component/Sliders.jsx
+++ src/component/Sliders.jsx
@@ -1,44 +1,44 @@
 import React from 'react'
 import "./style.css"
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { FcOk } from "react-icons/fc";
 
 function Sliders() {
 
-  const renderSlide = (imageUrl, altText, legend) => {
-    return (
-      <div>
-        <img className="custom-image" src={imageUrl} alt={altText} />
-        <p className="legend">{legend}</p>
-      </div>
-    );
-  };
+
 
   return (
     <>
-    <Carousel autoPlay interval={3000} infinite showThumbs={false} renderItem={renderSlide}>
-      {[
-        {
-          imageUrl: 'https://www.manipalhospitals.com/campaign/hospitals/pune-baner/img/jaipur-facility1.webp',
-          altText: 'Image 1',
-          legend: 'Legend 1'
-        },
-        {
-          imageUrl: 'https://www.manipalhospitals.com/campaign/hospitals/pune-baner/img/jaipur-facility1.webp',
-          altText: 'Image 2',
-          legend: 'Legend 2'
-        },
-        {
-          imageUrl: 'https://www.manipalhospitals.com/campaign/hospitals/pune-baner/img/jaipur-facility1.webp',
-          altText: 'Image 3',
-          legend: 'Legend 3'
-        }
-      ].map((slide, index) => (
-        <div key={index}>{renderSlide(slide.imageUrl, slide.altText, slide.legend)}</div>
-      ))}
-    </Carousel>
+      <div className='m-10 flex flex-row'>
+        <div className='w-40%'>
+          <img src="https://www.manipalhospitals.com/campaign/hospitals/pune-baner/img/image_ortho.png" alt="..." className='flip-animation ' />
+        </div>
+        <div className='w-3/4 mr-60 slide-in '>
+          <div className='ml-12'>
+            <h1 className='font-bold text-2xl text-indigo-800'>Orthopaedics</h1>
+            <p className='mt-8 text-base font-serif'>Orthopaedics is the branch of medicine that deals with the prevention or correction of deformities, disorders and injuries of the musculoskeletal system. Orthopaedic surgeons use a variety of techniques to treat conditions such as broken bones, torn ligaments, sprains, strains, joint problems, and congenital deformities. Orthopaedic surgery can be performed on patients of all ages, from infants to the elderly. In most cases, orthopaedic surgery is performed on an outpatient basis, which means the patient can go home the same day. However, some procedures may require a brief hospital stay. The goal of orthopaedic surgery is to restore the function and mobility of the musculoskeletal system. Orthopaedic surgeons work closely with other medical professionals, such as physical therapists, to ensure that patients receive the best possible care.</p>
+
+            <div className='mt-6'>
+              <div className='flex flex-row mt-8 text-base font-serif '>
+                <p className='flex flex-row'>  <FcOk className='mt-1 mr-5' /> Sports Injury Treatment</p>
+                <p className='flex flex-row ml-20'>  <FcOk className='mt-1 mr-5' /> Sports Injury Treatment</p>
+              </div>
+              <div className='flex flex-row mt-2 text-base font-serif '>
+                <p className='flex flex-row'>  <FcOk className='mt-1 mr-5' /> Sports Injury Treatment</p>
+                <p className='flex flex-row ml-20'>  <FcOk className='mt-1 mr-5' /> Sports Injury Treatment</p>
+              </div>
+              <div className='flex flex-row mt-2 text-base font-serif '>
+                <p className='flex flex-row'>  <FcOk className='mt-1 mr-5' /> Sports Injury Treatment</p>
+                <p className='flex flex-row ml-20'>  <FcOk className='mt-1 mr-5' /> Sports Injury Treatment</p>
+              </div>
+            </div>
+            <div className='bg-gradient-to-r  from-blue-900 from-10% via-sky-700 via-30% to-emerald-600 to-90% w-56 py-2 mt-10 rounded cursor-pointer flip-button'>
+              <p className='p-1 text-l font-serif text-white text-center '>Book An Appointment</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  
+
   )
 }
 
