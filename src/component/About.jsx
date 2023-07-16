1bfc3fd85a23af0940f9b3c5cd402f48cdffb86b
Chandrakant <chandrakantgawali92@gmail.com>
2023-07-16 14:21:50 +0530

    Add about

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -9,16 +9,19 @@ function About() {
             {/* <div>About</div> */}
             <div className='flex flex-row  mb-10'>
                 <div className='ml-52 mr-40'>
-                    <h1 className='font-bold text-2xl text-indigo-800'>Why Choose Manipal ?</h1>
+                    <h1 className='font-bold text-2xl text-indigo-800'>Embrace the Power of Natural Healing </h1>
 
-                    <p className='mt-8 text-lg font-serif'>Manipal Hospital Baner, is one of the leading multi-specialty tertiary care hospitals in India. The hospital has been providing world-class medical care to patients worldwide for over three decades. The hospital offers a wide range of treatments and services, including cardiology, oncology, orthopaedics, and more. Manipal Hospital Baner, is also home to a state-of-the-art cancer center, which offers the latest treatments and technologies for cancer patients. Book an appointment to consult with our experts.</p>
+                    <p className='mt-8 text-lg font-serif'>AiSense in the ST version for the structures will provide indicators on the state of health, suggest diagnosis, offer services for monitoring pathologies and therapies.
+                        Allows doctors to consult the anonymized database with evidence of patients with similar characteristics, diagnosis. Allows you to enter data from other analyzes or clinical evidence; allows you to consult the patient’s history and related deductions made by the expert system of AiSense also via Chat BOT (for the collection of further wellness data or disturbances reported by the patient).</p>
 
-                    <p className='mt-4 text-lg font-serif'>The hospital has a team of highly skilled and experienced doctors who are committed to providing the best possible care to their patients. Manipal Hospital Baner, is also home to a team of dedicated nurses who provide 24-hour care to patients. The hospital has a world-class infrastructure, which includes a well-equipped emergency department, advanced diagnostic facilities, and a modern laboratory. Manipal Hospital Baner, also has a dedicated team of support staff who are always ready to help patients and their families.</p>
+                    <p className='mt-4 text-lg font-serif'>➤ At AiSense Naturopathy Online Consultancy, we believe that true healing comes from within. By integrating the principles of naturopathy into your life, you can unlock the body's innate potential to achieve and maintain optimal health. Our dedicated team is here to guide you every step of the way on your journey to wellness.</p>
+                    <p className='mt-4 text-lg font-serif'> ➤ Take the first step towards a balanced and vibrant life. Book your online naturopathy consultation with us today and discover the transformative power of natural healing..</p>
+                    <p className='mt-4 text-lg font-serif'> ➤ Join AiSense LLP in our commitment to promoting holistic well-being and empowering individuals to live healthier, happier lives. Together, let's embrace the wisdom of nature and unlock your body's innate healing potentia.</p>
                     <div className='bg-gradient-to-r  from-blue-900 from-10% via-sky-700 via-30% to-emerald-600 to-90% w-36 px-5 mt-10 rounded cursor-pointer flip-animation'>
                         <p className='p-1 text-l font-serif text-white '>Book An Appointment</p>
                     </div>
                 </div>
-                <div className=' bg-[#19aba7] mr-56 px-8 pb-10'>
+                {/* <div className=' bg-[#19aba7] mr-56 px-8 pb-10'>
                     <div className='flex flex-row  pt-10 '>
                         <div className=''>
                             <div class="w-10 h-10 bg-gradient-to-r from-indigo-800 to-[#19aba6fa] rounded-full relative right-14 mt-3 "></div>
@@ -31,8 +34,8 @@ function About() {
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className='flex flex-row  '>
+                    </div> */}
+                    {/* <div className='flex flex-row  '>
                         <div className=''>
                             <div class="w-10 h-10 bg-gradient-to-r from-indigo-800 to-[#19aba6fa] rounded-full relative right-14  mt-3  "></div>
                         </div>
@@ -44,8 +47,8 @@ function About() {
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className='flex flex-row  '>
+                    </div> */}
+                    {/* <div className='flex flex-row  '>
                         <div className=''>
                             <div class="w-10 h-10 bg-gradient-to-r from-indigo-800 to-[#19aba6fa] rounded-full relative right-14  mt-3  "></div>
                         </div>
@@ -96,10 +99,14 @@ function About() {
                                 </div>
                             </div>
                         </div>
+                    </div> */}
+
+
+                {/* </div> */}
+                    <div className='mr-56 mt-36 pb-10'>
+                        <img className=' ' style={{width:3000, height:500 }}  src="https://aisense.co.in/assets/img/healing.jpg" alt="" />
                     </div>
 
-
-                </div>
 
             </div>
             <br />
